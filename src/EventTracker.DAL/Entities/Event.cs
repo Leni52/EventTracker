e2e1213e6eb053368f36e48bc865c13f63fc2018
Georgi Blazhev <georgi.blazhev@scalefocus.com>
2022-04-20 12:27:39 +0300

    adding Event entity and Category enum

---
+++ src/EventTracker.DAL/Entities/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventTracker.DAL.Entities
+{
+    public class Event : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Category Category { get; set; }
+        public string Location { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
