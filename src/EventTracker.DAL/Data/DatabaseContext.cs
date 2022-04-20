a9647fc0baa06246363ab7d3f0ccdc1d313c1f3b
Georgi Blazhev <georgi.blazhev@scalefocus.com>
2022-04-20 12:34:23 +0300

    Adding Comment Entity and DatabaseContext

---
+++ src/EventTracker.DAL/Data/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using EventTracker.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventTracker.DAL.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
