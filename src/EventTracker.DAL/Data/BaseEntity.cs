1ea0d71171d88301a537c11716aa448bb2afce8e
Georgi Blazhev <georgi.blazhev@scalefocus.com>
2022-04-20 12:18:09 +0300

    adding BaseEntity abstract class

---
+++ src/EventTracker.DAL/Data/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventTracker.DAL.Data
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastModifiedAt { get; set; }
+    }
+}
