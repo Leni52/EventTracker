58b85cd2dcb11a601438bd2c8d19dbe86a67574c
Georgi Blazhev <georgi.blazhev@scalefocus.com>
2022-04-20 12:36:42 +0300

    creating ExternalUser class

--- src/EventTracker.DAL/Entities/BaseEntity.cs
+++ src/EventTracker.DAL/Entities/BaseEntity.cs
@@ -9,7 +9,7 @@ namespace EventTracker.DAL.Entities
     public abstract class BaseEntity
     {
         public Guid Id { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
-        public DateTime LastModifiedAt { get; set; } = DateTime.Now;
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastModifiedAt { get; set; }
     }
 }
