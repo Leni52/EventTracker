23f597260bb7429770dc5ab7d89943cb0edfe3ad
Nikolay Shtipkov <nikolay.shtipkov@scalefocus.com>
2022-04-27 17:44:04 +0300

    initialize event service file

---
+++ src/EventTracker.BLL/Services/EventService.cs
@@ -0,0 +1,19 @@
+﻿using EventTracker.DAL.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventTracker.BLL.Services
+{
+    public class EventService
+    {
+        private readonly DatabaseContext _context;
+
+        public EventService(DatabaseContext context)
+        {
+            _context = context;
+        }
+    }
+}
