2d68946e78dec3ceba14914db2b897f5810f6e86
Nikolay Shtipkov <nikolay.shtipkov@scalefocus.com>
2022-04-28 15:39:53 +0300

    implement post/create event logic

--- src/EventTracker.BLL/Services/EventService.cs
+++ src/EventTracker.BLL/Services/EventService.cs
@@ -1,5 +1,6 @@
 ﻿using EventTracker.DAL.Data;
 using EventTracker.DAL.Entities;
+using EventTracker.DTO.Requests;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -26,6 +27,23 @@ namespace EventTracker.BLL.Services
         public async Task<Event> GetEventByIdAsync(Guid eventId)
         {
             return await _context.Events.FirstOrDefaultAsync(e => e.Id == eventId);
+        }
+
+        public async Task CreateEventAsync(EventRequestDTO eventRequest)
+        {
+            var eventToCreate = new Event()
+            {
+                Name = eventRequest.Name,
+                Description = eventRequest.Description,
+                Location = eventRequest.Location,
+                StartDate = eventRequest.StartDate,
+                EndDate = eventRequest.EndDate,
+                CreatedAt = DateTime.Now,
+                LastModifiedAt = DateTime.Now
+            };
+
+            await _context.Events.AddAsync(eventToCreate);
+            await _context.SaveChangesAsync();
         }
     }
 }
