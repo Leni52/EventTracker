b5a724a448c2d9e05849ea4ce1c6648d35c13937
Nikolay Shtipkov <nikolay.shtipkov@scalefocus.com>
2022-04-29 13:37:45 +0300

    add map event and get operations in event controller

--- src/EventTracker/Controllers/EventController.cs
+++ src/EventTracker/Controllers/EventController.cs
@@ -1,5 +1,10 @@
 ﻿using EventTracker.BLL.Interfaces;
+using EventTracker.DAL.Entities;
+using EventTracker.DTO.Responses;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace EventTracker.Controllers
 {
@@ -13,6 +18,42 @@ namespace EventTracker.Controllers
         public EventsController(IEventService eventService) : base()
         {
             _eventService = eventService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<EventReponseDTO>>> GetAllEventsAsync()
+        {
+            var events = await _eventService.GetAllEventsAsync();
+            var eventsResponse = new List<EventReponseDTO>();
+            foreach (var e in events)
+            {
+                eventsResponse.Add(MapEvent(e));
+            }
+
+            return eventsResponse;
+        }
+
+        [HttpGet("{eventId}")]
+        public async Task<ActionResult<EventReponseDTO>> GetEventByIdAsync(Guid eventId)
+        {
+            var eventById = await _eventService.GetEventByIdAsync(eventId);
+
+            return MapEvent(eventById);
+        }
+
+        private EventReponseDTO MapEvent(Event eventEntity)
+        {
+            var eventMap = new EventReponseDTO()
+            {
+                Name = eventEntity.Name,
+                Description = eventEntity.Description,
+                Location = eventEntity.Location,
+                Category = eventEntity.Category,
+                StartDate = eventEntity.StartDate,
+                EndDate = eventEntity.EndDate,
+            };
+
+            return eventMap;
         }
     }
 }
