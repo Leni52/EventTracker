71b4493c382a0a0b3d7646f6ad80d9440097ebbe
Nikolay Shtipkov <nikolay.shtipkov@scalefocus.com>
2022-04-29 13:16:39 +0300

    init events controller file

---
+++ src/EventTracker/Controllers/EventController.cs
@@ -0,0 +1,18 @@
+﻿using EventTracker.BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventTracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class EventsController : ControllerBase
+    {
+        private static IEventService _eventService;
+
+        public EventsController(IEventService eventService) : base()
+        {
+            _eventService = eventService;
+        }
+    }
+}
