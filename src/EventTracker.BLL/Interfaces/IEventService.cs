31c57685742cfa2acd8d972c8d3429839be2d7c0
Nikolay Shtipkov <nikolay.shtipkov@scalefocus.com>
2022-04-29 13:10:34 +0300

    add interface file for event service

---
+++ src/EventTracker.BLL/Interfaces/IEventService.cs
@@ -0,0 +1,17 @@
+﻿using EventTracker.DAL.Entities;
+using EventTracker.DTO.Requests;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EventTracker.BLL.Interfaces
+{
+    public interface IEventService
+    {
+        Task CreateEventAsync(EventRequestDTO eventRequest);
+        Task DeleteEventAsync(Guid eventId);
+        Task<List<Event>> GetAllEventsAsync();
+        Task<Event> GetEventByIdAsync(Guid eventId);
+        Task UpdateEventAsync(EventRequestDTO eventRequest, Guid eventId);
+    }
+}
