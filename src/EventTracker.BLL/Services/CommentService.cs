01a478b834fab9ffebebd56fafb5195ce6fba85a
Nikolay Shtipkov <nikolay.shtipkov@scalefocus.com>
2022-05-04 10:31:18 +0300

    init comment service file

---
+++ src/EventTracker.BLL/Services/CommentService.cs
@@ -0,0 +1,19 @@
+﻿using EventTracker.DAL.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventTracker.BLL.Services
+{
+    public class CommentService
+    {
+        private readonly DatabaseContext _context;
+
+        public CommentService(DatabaseContext context)
+        {
+            _context = context;
+        }
+    }
+}
