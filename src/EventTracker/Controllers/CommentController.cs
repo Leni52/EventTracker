b6b4a83dfe883f2efe555c383f52ae393beef411
Nikolay Shtipkov <nikolay.shtipkov@scalefocus.com>
2022-05-04 11:53:53 +0300

    implement post/put/delete operations in comment controller

--- src/EventTracker/Controllers/CommentController.cs
+++ src/EventTracker/Controllers/CommentController.cs
@@ -1,5 +1,6 @@
 ﻿using EventTracker.BLL.Interfaces;
 using EventTracker.DAL.Entities;
+using EventTracker.DTO.Requests;
 using EventTracker.DTO.Responses;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -33,11 +34,47 @@ namespace EventTracker.Controllers
         }
 
         [HttpGet("{commentId}")]
-        public async Task<ActionResult<CommentResponseDTO>> GetEventByIdAsync(Guid commentId)
+        public async Task<ActionResult<CommentResponseDTO>> GetCommentByIdAsync(Guid commentId)
         {
             var commentById = await _commentService.GetCommentByIdAsync(commentId);
 
             return MapComment(commentById);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCommentAsync(CommentRequestDTO commentRequest)
+        {
+            await _commentService.CreateCommentAsync(commentRequest);
+            if (ModelState.IsValid)
+            {
+                return Ok("Comment created successfully.");
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPut("{commentId}")]
+        public async Task<IActionResult> UpdateCommentAsync(CommentRequestDTO commentRequest, Guid commentId)
+        {
+            await _commentService.UpdateCommentAsync(commentRequest, commentId);
+            if (ModelState.IsValid)
+            {
+                return Ok("Comment updated successfully.");
+            }
+
+            return BadRequest();
+        }
+
+        [HttpDelete("{commentId}")]
+        public async Task<IActionResult> DeleteCommentAsync(Guid commentId)
+        {
+            await _commentService.DeleteCommentAsync(commentId);
+            if (ModelState.IsValid)
+            {
+                return Ok("Comment deleted successfully.");
+            }
+
+            return BadRequest();
         }
 
         private CommentResponseDTO MapComment(Comment commentEntity)
