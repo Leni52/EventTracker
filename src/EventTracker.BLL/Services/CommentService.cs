1ce791bc458fea4eb00256d670102b00d117ea63
Nikolay Shtipkov <nikolay.shtipkov@scalefocus.com>
2022-05-04 11:25:20 +0300

    implement create/update/remove comment logic and modified comment entity and dto

--- src/EventTracker.BLL/Services/CommentService.cs
+++ src/EventTracker.BLL/Services/CommentService.cs
@@ -1,5 +1,6 @@
 ﻿using EventTracker.DAL.Data;
 using EventTracker.DAL.Entities;
+using EventTracker.DTO.Requests;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -26,6 +27,47 @@ namespace EventTracker.BLL.Services
         public async Task<Comment> GetCommentByIdAsync(Guid commentId)
         {
             return await _context.Comments.FirstOrDefaultAsync(c => c.Id == commentId);
+        }
+
+        public async Task CreateCommentAsync(CommentRequestDTO commentRequest)
+        {
+            var commentToCreate = new Comment()
+            {
+                Text = commentRequest.Text,
+                EventId = commentRequest.EventId,
+                CreatedAt = DateTime.Now,
+                LastModifiedAt = DateTime.Now
+            };
+
+            await _context.Comments.AddAsync(commentToCreate);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateCommentAsync(CommentRequestDTO commentRequest, Guid commentId)
+        {
+            var commentToUpdate = await _context.Comments.FirstOrDefaultAsync(e => e.Id == commentId);
+            if (commentToUpdate == null)
+            {
+                throw new Exception("Event doesn't exist.");
+            }
+
+            commentToUpdate.Text = commentRequest.Text;
+            commentToUpdate.LastModifiedAt = DateTime.Now;
+
+            _context.Comments.Update(commentToUpdate);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteCommentAsync(Guid commentId)
+        {
+            var commentToDelete = await GetCommentByIdAsync(commentId);
+            if (commentToDelete == null)
+            {
+                throw new Exception("Event doesn't exist.");
+            }
+
+            _context.Comments.Remove(commentToDelete);
+            await _context.SaveChangesAsync();
         }
     }
 }
