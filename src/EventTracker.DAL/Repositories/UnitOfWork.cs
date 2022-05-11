558f144bdb5d059f10ecd739e6840f78ceea7e3e
Nikolay Shtipkov <nikolay.shtipkov@scalefocus.com>
2022-05-11 14:38:10 +0300

    init unit of work class

---
+++ src/EventTracker.DAL/Repositories/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using EventTracker.DAL.Contracts;
+using EventTracker.DAL.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventTracker.DAL.Repositories
+{
+    public class UnitOfWork
+    {
+        private readonly DatabaseContext _context;
+        private IEventRepository _eventRepository;
+        private ICommentRepository _commentRepository;
+
+        public UnitOfWork(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IEventRepository Events
+            => _eventRepository ??= new EventRepository(_context);
+        public ICommentRepository Comments
+            => _commentRepository ??= new CommentRepository(_context);
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
