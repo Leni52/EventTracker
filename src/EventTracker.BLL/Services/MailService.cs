bd6280bbd2427199c5e7a9e01543a5c52587d46b
Atanasia Vasileva <atanasia.vasileva@scalefocus.com>
2022-05-12 15:52:46 +0300

    Changed host

--- src/EventTracker.BLL/Services/MailService.cs
+++ src/EventTracker.BLL/Services/MailService.cs
@@ -37,7 +37,7 @@ namespace EventTracker.BLL.Services
 
             using var smtp = new SmtpClient();
 
-            smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
+            smtp.Connect(_mailSettings.Host, _mailSettings.Port);
 
             smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
 
