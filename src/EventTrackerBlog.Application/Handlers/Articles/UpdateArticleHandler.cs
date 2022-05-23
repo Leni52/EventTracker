8d11ac6edeac9b01c90c0d697edc6dd3be93dd00
Lenche Petrov <lenche.petrov@scalefocus.com>
2022-05-23 11:59:33 +0300

    add dto for update article

--- src/EventTrackerBlog.Application/Handlers/Articles/UpdateArticleHandler.cs
+++ src/EventTrackerBlog.Application/Handlers/Articles/UpdateArticleHandler.cs
@@ -1,6 +1,8 @@
-﻿using EventTrackerBlog.Application.Exceptions;
+﻿using AutoMapper;
+using EventTrackerBlog.Application.Exceptions;
 using EventTrackerBlog.Application.Features.Articles.Commands;
 using EventTrackerBlog.Domain.Data;
+using EventTrackerBlog.Domain.DTO.Articles.Response;
 using EventTrackerBlog.Domain.Entities;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -11,14 +13,16 @@ using System.Threading.Tasks;
 
 namespace EventTrackerBlog.Application.Handlers.Articles
 {
-    public class UpdateArticleCommandHandler : IRequestHandler<UpdateArticleCommand, Guid>
+    public class UpdateArticleCommandHandler : IRequestHandler<UpdateArticleCommand, ArticleResponseModel>
     {
         private readonly BlogDbContext _context;
-        public UpdateArticleCommandHandler(BlogDbContext context)
+        private readonly IMapper _mapper;
+        public UpdateArticleCommandHandler(BlogDbContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
-        public async Task<Guid> Handle(UpdateArticleCommand command, CancellationToken cancellationToken)
+        public async Task<ArticleResponseModel> Handle(UpdateArticleCommand command, CancellationToken cancellationToken)
         {
             var articleToUpdate = await _context.Articles.Where(a => a.Id == command.ArticleId).FirstOrDefaultAsync();
             if (articleToUpdate == null)
@@ -28,7 +32,9 @@ namespace EventTrackerBlog.Application.Handlers.Articles
             articleToUpdate.Title = command.Title;
             articleToUpdate.Content = command.Content;
             articleToUpdate.LastModifiedAt = DateTime.Now;
-            return articleToUpdate.Id;
+            _context.SaveChanges();
+            var articleResponse = _mapper.Map<ArticleResponseModel>(articleToUpdate);
+            return articleResponse;
         }
     }
 }
