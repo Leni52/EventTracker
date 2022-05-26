7a085dca9af24714e2ad94a9d0eaa1c58b04e59c
Lenche Petrov <lenche.petrov@scalefocus.com>
2022-05-26 15:38:24 +0300

    remove unneccesary registering services in startup

--- src/EventTrackerBlog.WebAPI/Startup.cs
+++ src/EventTrackerBlog.WebAPI/Startup.cs
@@ -1,11 +1,4 @@
-using EventTrackerBlog.Application.Features.Articles.Commands;
-using EventTrackerBlog.Application.Features.Articles.Queries;
-using EventTrackerBlog.Application.Features.Comments.Commands;
-using EventTrackerBlog.Application.Features.Comments.Queries;
 using EventTrackerBlog.Domain.Data;
-using EventTrackerBlog.Domain.DTO.Articles.Request;
-using EventTrackerBlog.Domain.DTO.Articles.Response;
-using EventTrackerBlog.Domain.DTO.Comments.Response;
 using EventTrackerBlog.Domain.Seed;
 using ExceptionHandling.Handler;
 using MediatR;
@@ -16,19 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
 using static EventTrackerBlog.Application.Features.Articles.Commands.CreateArticle;
-using static EventTrackerBlog.Application.Features.Articles.Commands.DeleteArticleById;
-using static EventTrackerBlog.Application.Features.Articles.Commands.UpdateArticle;
-using static EventTrackerBlog.Application.Features.Articles.Queries.GetAllArticles;
-using static EventTrackerBlog.Application.Features.Articles.Queries.GetArticleById;
-using static EventTrackerBlog.Application.Features.Comments.Commands.CreateComment;
-using static EventTrackerBlog.Application.Features.Comments.Commands.DeleteComment;
-using static EventTrackerBlog.Application.Features.Comments.Commands.EditComment;
-using static EventTrackerBlog.Application.Features.Comments.Queries.GetAllComments;
-using static EventTrackerBlog.Application.Features.Comments.Queries.GetCommentById;
-using static EventTrackerBlog.Application.Features.Comments.Queries.GetCommentsByArticle;
 
 namespace EventTrackerBlog.WebAPI
 {
@@ -58,21 +39,8 @@ namespace EventTrackerBlog.WebAPI
                 options.UseSqlServer(Configuration["ConnectionStrings:Default"]));
 
             //register handlers and queries/commands
+            services.AddMediatR(typeof(CreateArticleHandler));
 
-            services.AddScoped<IRequestHandler<GetAllComments, IEnumerable<CommentResponseModel>>, GetAllCommentsHandler>();
-            services.AddScoped<IRequestHandler<GetCommentsByArticle, IEnumerable<CommentResponseModel>>, GetCommentsByArticleHandler>();
-            services.AddScoped<IRequestHandler<GetCommentById, CommentResponseModel>, GetCommentByIdHandler>();
-            services.AddScoped<IRequestHandler<CreateComment, CommentResponseModel>, CreateCommentHandler>();
-            services.AddScoped<IRequestHandler<EditComment, CommentResponseModel>, EditCommentHandler>();
-            services.AddScoped<IRequestHandler<DeleteComment, Guid>, DeleteCommentHandler>();
-
-            services.AddScoped<IRequestHandler<GetAllArticles, IEnumerable<ArticleResponseModel>>, GetAllArticlesHandler>();
-            services.AddScoped<IRequestHandler<GetArticleById, ArticleResponseModel>, GetArticleByIdHandler>();
-            services.AddScoped<IRequestHandler<CreateArticle, ArticleRequestModel>, CreateArticleHandler>();
-            services.AddScoped<IRequestHandler<DeleteArticleById, Guid>, DeleteArticleByIdHandler>();
-            services.AddScoped<IRequestHandler<UpdateArticle, ArticleResponseModel>, UpdateArticleHandler>();
-
-            services.AddMediatR(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
