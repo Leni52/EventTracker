c1db56644dd274dc8266ffc4240ad66c7c48d7d6
Atanasia Vasileva <atanasia.vasileva@scalefocus.com>
2022-05-27 09:35:33 +0300

    started writing comment controller unit tests

--- src/EventTrackerBlog.Tests/Comments/Controller/CommentControllerTests.cs
+++ src/EventTrackerBlog.Tests/Comments/Controller/CommentControllerTests.cs
@@ -1,7 +1,17 @@
-﻿namespace EventTrackerBlog.Tests.Comments.Controller
+﻿using EventTrackerBlog.WebAPI.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using MyTested.AspNetCore.Mvc;
+using Xunit;
+
+namespace EventTrackerBlog.Tests.Comments.Controller
 {
     public class CommentControllerTests
     {
-
+        [Fact]
+        public void ControllerShouldHaveCorrectAttributes()
+            => MyController<CommentController>
+                .Instance()
+                .ShouldHave()
+                .Attributes(x => x.ContainingAttributeOfType<ApiControllerAttribute>());
     }
 }
