2c420cabc042a29cec0874feafbb44ca5fcc80e3
Nikolay Shtipkov <nikolay.shtipkov@scalefocus.com>
2022-05-30 13:15:23 +0300

    implement tests for get/list comment operations

--- src/EventTracker.BLL.UnitTests/CommentServiceTest.cs
+++ src/EventTracker.BLL.UnitTests/CommentServiceTest.cs
@@ -4,8 +4,6 @@ using ExceptionHandling.Exceptions;
 using Moq;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -75,6 +73,44 @@ namespace EventTracker.BLL.UnitTests
 
             await Assert.ThrowsAsync<ItemDoesNotExistException>(async () => await sut
                 .DeleteCommentAsync(new Guid()));
+        }
+
+        [Fact]
+        public async Task GetAllCommentsAsync_SuccessfullyReturnsCollection()
+        {
+            var sut = new CommentService(unitOfWork.Object);
+            var comments = new List<Comment>();
+
+            unitOfWork.Setup(uof => uof.Comments.GetAllAsync()).ReturnsAsync(comments);
+
+            var result = await sut.GetAllCommentsAsync();
+
+            Assert.Equal(result, comments);
+        }
+
+        [Fact]
+        public async Task GetCommentByIdAsync_SuccessfullyReturnsEvent()
+        {
+            var sut = new CommentService(unitOfWork.Object);
+            var comment = new Comment();
+
+            unitOfWork.Setup(uof => uof.Comments.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(comment);
+
+            var result = await sut.GetCommentByIdAsync(new Guid());
+
+            Assert.Equal(result, comment);
+        }
+
+        [Fact]
+        public async Task GetEventByIdAsync_ThrowsItemDoesNotExistException()
+        {
+            var sut = new CommentService(unitOfWork.Object);
+
+            Comment comment = null;
+
+            unitOfWork.Setup(uof => uof.Comments.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(comment);
+
+            await Assert.ThrowsAsync<ItemDoesNotExistException>(async () => await sut.GetCommentByIdAsync(new Guid()));
         }
     }
 }
