56fcbe239ba4a52b1f57baccc9c87cb66698ec8a
Lenche Petrov <lenche.petrov@scalefocus.com>
2022-05-31 14:47:36 +0300

    writing to text file

--- src/EventTrackerBlog.WebAPI/Filters/ActionFilterAttribute.cs
+++ src/EventTrackerBlog.WebAPI/Filters/ActionFilterAttribute.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Logging;
 using System;
+using System.IO;
 
 namespace EventTrackerBlog.WebAPI.Filters
 {
@@ -11,19 +12,31 @@ namespace EventTrackerBlog.WebAPI.Filters
         {
             _logger = logger;
         }
+
         public void OnActionExecuting(ActionExecutingContext context)
         {
             _logger.LogInformation("On ActionExecuting method: ");
             var controllerName = context.RouteData.Values["controller"].ToString();
             var actionName = context.RouteData.Values["action"].ToString();
             _logger.LogInformation($"Controller: {controllerName}, Action Name: {actionName}");
-
+            Log("log.txt", $"Controller: {controllerName}, Action Name: {actionName}");
         }
+
         public void OnActionExecuted(ActionExecutedContext context)
         {
             _logger.LogInformation("On ActionExecuted method: ");
+            Log("log.txt", "OnActionExecuted method executed.");
         }
 
-
+        public void Log(string fileName, string content)
+        {
+            using (StreamWriter w = File.AppendText(fileName))
+            {
+                w.Write("\r\nLog Entry : ");
+                w.WriteLine($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}");
+                w.WriteLine(content);
+                w.WriteLine("-------------------------------");
+            }
+        }
     }
 }
