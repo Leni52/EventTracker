289c53030a1c1ce10d4b58424f55f1b1dc26bd5c
Nikolay Shtipkov <nikolay.shtipkov@scalefocus.com>
2022-06-13 16:11:45 +0300

    change endpoint response so it doesnt throw and error when creating a comment

--- src/angular-front/src/app/modules/comments/services/comments/comments-routing.module.ts
+++ src/angular-front/src/app/modules/comments/services/comments/comments-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AllCommentsComponent } from '../../pages/all-comments/all-comments.component';
+import { CreateCommentComponent } from '../../pages/create-comment/create-comment.component';
 
 const routes: Routes = [
-  {path: 'events/:id/comments', component: AllCommentsComponent}
+  {path: 'events/:id/comments', component: AllCommentsComponent},
+  {path: 'createComment', component: CreateCommentComponent}
 ];
 
 @NgModule({
