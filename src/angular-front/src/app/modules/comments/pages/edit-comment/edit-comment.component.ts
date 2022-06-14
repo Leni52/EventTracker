d83f8fe1c9c9c1d34a5ed8810dbf244bd30497d0
Lenche Petrov <lenche.petrov@scalefocus.com>
2022-06-14 10:01:35 +0300

    feat: added EditComment component, added GetCommentById method, fixed few routes

---
+++ src/angular-front/src/app/modules/comments/pages/edit-comment/edit-comment.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CommentModelRequest } from '../../models/request/CommentModelRequest';
+import { CommentModelResponse } from '../../models/response/CommentModelResponse';
+import { CommentsService } from '../../services/comments.service';
+
+@Component({
+  selector: 'app-edit-comment',
+  templateUrl: './edit-comment.component.html',
+  styleUrls: ['./edit-comment.component.css']
+})
+export class EditCommentComponent implements OnInit {
+  id!: string;
+  eventId!: string;
+  comment!: CommentModelResponse;
+
+  commentModels: CommentModelResponse[]=[];
+  editCommentForm!: FormGroup;
+
+  constructor(
+    public commentService: CommentsService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { 
+    this.editCommentForm = this.formBuilder.group({
+      id:[''],
+      eventId:[''],
+      text:['']
+    })
+  }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.commentService.getAllCommentsFromEvent(this.eventId).subscribe((data: CommentModelResponse[])=>{
+      this.commentModels = data;
+    });
+
+    this.commentService.getCommentById(this.id).subscribe((data:CommentModelResponse)=>{
+      this.comment = data;
+      this.editCommentForm.patchValue(data);
+    })
+  }
+
+  onSubmit(formData: { value: CommentModelRequest; }) {
+    this.commentService.editComment(this.id, formData.value).subscribe(res=>{
+      this.router.navigateByUrl('/events');
+    })
+  }
+
+}
