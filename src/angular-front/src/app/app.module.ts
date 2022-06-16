c53d53c444a55e527fe6ace7d44f8054c77be349
Lenche Petrov <lenche.petrov@scalefocus.com>
2022-06-16 15:59:52 +0300

    feat: added loader component, service and interceptor

--- src/angular-front/src/app/app.module.ts
+++ src/angular-front/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
@@ -21,6 +21,9 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoaderComponent } from './shared/pages/loader/loader.component';
+import {MatProgressSpinner, MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { LoaderInterceptor } from './shared/interceptors/loader.interceptor';
 
 @NgModule({
   declarations: [
@@ -36,8 +39,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     CreateCommentComponent,
     EditCommentComponent,
     ConfirmationComponent,
+    LoaderComponent,
   ],
   imports: [
+   
     BrowserModule,
     HttpClientModule,
     FormsModule,
@@ -70,9 +75,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatButtonModule,
     MatToolbarModule,
     BrowserAnimationsModule,
+    MatProgressSpinnerModule
   ],
   exports: [RouterModule],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoaderInterceptor,
+      multi:true,
+    }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
